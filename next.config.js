81161d3199ef3f091f8142cca4002bdd2a69fd5b
JAYPATRICK MANALANSAN <jaypatrickm@gmail.com>
2020-03-25 17:29:41 -0700

    revert config change

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 require('dotenv').config();
 module.exports = {
-  distDir: '_next',
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
   env: {
     sendgrid_api_key: process.env.sendgrid_api_key
   }
